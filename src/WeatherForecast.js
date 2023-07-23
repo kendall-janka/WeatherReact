6ae910327829ad2abd525468cab3c13ecb44e846
kendall-janka <126126288+kendall-janka@users.noreply.github.com>
2023-07-23 16:11:34 -0400

    Add Forecast line and UseEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./WeatherForecast.css";
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
@@ -14,26 +26,40 @@ export default function WeatherForecast(props) {
     console.log(forecast);
     return (
       <div className="forecast">
-        <div className="row"></div>
-        <div className="col">
-          <div className="forecastDay"> {forecast[0].dt} </div>
-          <div className="forecastIcon"></div>
-          <img
-            className="currentIcon"
-            alt="sunny"
-            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-            width="40"
-            height="40"
-          ></img>{" "}
-        </div>
-        <div className="forecastTemps">
-          <span className="min-temp">
-            {Math.round(forecast[0].temperature.minimum)}°
-          </span>
-          |
-          <span className="max-temp">
-            {Math.round(forecast[0].temperature.maximum)}°
-          </span>
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col">
+                  <div>
+                    <div className="forecastDay">
+                      {" "}
+                      {formatDay(forecast[index].time)}{" "}
+                    </div>
+                    <div className="forecastIcon"></div>
+                    <img
+                      className="currentIcon"
+                      alt="sunny"
+                      src={forecast[index].condition.icon_url}
+                      width="40"
+                      height="40"
+                    ></img>{" "}
+                  </div>
+                  <div className="forecastTemps">
+                    <span className="min-temp">
+                      {Math.round(forecast[index].temperature.minimum)}°
+                    </span>
+                    |
+                    <span className="max-temp">
+                      {Math.round(forecast[index].temperature.maximum)}°
+                    </span>
+                  </div>
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
